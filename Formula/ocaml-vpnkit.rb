2c95f1994a19c59bdf60f60e61691fe683b14d4d
Stephane Lacoin (aka nxmatic) <slacoin@nuxeo.com>
2020-11-27 09:45:51 +0100

    opam: update 1.2.2_4 bottle. (clone)

---
+++ Formula/ocaml-vpnkit.rb
@@ -0,0 +1,63 @@
+# OCaml does not preserve binary compatibility across compiler releases,
+# so when updating it you should ensure that all dependent packages are
+# also updated by incrementing their revisions.
+#
+# Specific packages to pay attention to include:
+# - camlp4
+# - opam
+#
+# Applications that really shouldn't break on a compiler update are:
+# - mldonkey
+# - coq
+# - coccinelle
+# - unison
+class OcamlVpnkit < Formula
+  desc "General purpose programming language in the ML family"
+  homepage "https://ocaml.org/"
+  url "https://caml.inria.fr/pub/distrib/ocaml-4.07/ocaml-4.07.1.tar.xz"
+  sha256 "dfe48b1da31da9c82d77612582fae74c80e8d1ac650e1c24f5ac9059e48307b8"
+  revision 1
+
+  head "https://github.com/ocaml/ocaml.git", :branch => "trunk"
+
+  pour_bottle? do
+    # The ocaml compilers embed prefix information in weird ways that the default
+    # brew detection doesn't find, and so needs to be explicitly blacklisted.
+    reason "The bottle needs to be installed into /usr/local."
+    satisfy { HOMEBREW_PREFIX.to_s == "/usr/local" }
+  end
+
+  bottle do
+    cellar :any
+    sha256 "e570566296d4ebb49297d267aa01d46ef36cfcf2c4b88e57ac6eb8e1a2832423" => :mojave
+    sha256 "e662192a30ee7cc037f3665fa68680a35b00a4760c49fb12ccce01d61aa5a977" => :high_sierra
+    sha256 "ae0a24f0e8d52b1253d84703060bff3e1a3db657d882459936cde97c3834a357" => :sierra
+    sha256 "35d04c6e3b52a99b08e21ab0002992df2dd014a6ab53e857165893438591bf99" => :el_capitan
+  end
+
+  option "with-x11", "Install with the Graphics module"
+  option "with-flambda", "Install with flambda support"
+
+  depends_on :x11 => :optional
+
+  def install
+    ENV.deparallelize # Builds are not parallel-safe, esp. with many cores
+
+    # the ./configure in this package is NOT a GNU autoconf script!
+    args = ["-prefix", HOMEBREW_PREFIX.to_s, "-with-debug-runtime", "-mandir", man]
+    args << "-no-graph" if build.without? "x11"
+    args << "-flambda" if build.with? "flambda"
+    system "./configure", *args
+
+    system "make", "world.opt"
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    output = shell_output("echo 'let x = 1 ;;' | #{bin}/ocaml 2>&1")
+    assert_match "val x : int = 1", output
+    assert_match HOMEBREW_PREFIX.to_s, shell_output("#{bin}/ocamlc -where")
+  end
+end
+
+__END__
---
+++ README.md
@@ -0,0 +1,9 @@
+# Nxmatic Tap
+
+## How do I install these formulae?
+`brew install nxmatic/tap/<formula>`
+
+Or `brew tap nxmatic/tap` and then `brew install <formula>`.
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
